23e6162058bfae0ca1b0835e4debbe60feaebef8
GABRIELJOSEDASILVAA11bti <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>
2025-06-01 16:26:55 -0300

    criação do arquivo main.js e criação da função additem para adicionar itens no array

---
+++ main.js
@@ -0,0 +1,15 @@
+const items = []
+
+function addItem() {
+
+const itemName = document.querySelector("#item").value
+
+const item {
+    name: itemName,
+    checked: false,
+}
+
+items.push(item)
+
+documunet.querySelector("#item").value = ""
+}
